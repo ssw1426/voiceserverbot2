2f6205ea784f92ea6d950f3214f75a9de3c389d9
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-10 23:50:27 +0900

    Update Announcement_-_V10.py

--- Announcement_-_V10.py
+++ Announcement_-_V10.py
@@ -1,26 +1,26 @@
 import discord
 import asyncio
 import datetime
+import os
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('상메')
+    game = discord.Game('후원 기록 작성')
     await client.change_presence(status=discord.Status.online, activity=game)
     
 @client.event
 async def on_message(message):
-    if message.content.startswith('/공지'):
+    if message.content.startswith('!공지'):
                 try:
                     msg = message.content[4:]
-                    channel = 채널 ID
+                    channel = 💎후원-로그
                     if message.author.guild_permissions.manage_messages:
-                        await client.get_channel(channel).send("@everyone")
-                        embed = discord.Embed(title=" OO 공지사항", description= "```yaml" + "\n" + msg  + "```", color=0x1DDB16, timestamp=message.created_at)
+                        await
+                        embed = discord.Embed(title=" 후원 기록", description= "```yaml" + "\n" + msg  + "```", color=0x1DDB16, timestamp=message.created_at)
                         embed.set_footer(text=f"담당 관리자 :  {message.author}")
-                        embed.set_image(url="밑에 들어갈 사진 링크 안쓸꺼면 주석 하셈")
                         await client.get_channel(int(channel)).send(embed=embed)
                         await message.delete()
                     else:
@@ -29,5 +29,5 @@ async def on_message(message):
                 except:
                     pass
 
-
-client.run('봇 토근 넣으셈')
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
