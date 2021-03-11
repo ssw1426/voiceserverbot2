a083d0a593f95856a1581aa345890dcdeb1eacc0
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-11 10:57:48 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -14,11 +14,9 @@ async def on_message(message):
     if message.content.startswith('!후원'):
                 try:
                     msg = message.content[4:]
-                    channel = 818390444265832481
                     if message.author.guild_permissions.manage_messages:
-                        await client.get_channel(channel).send("@everyone")
                         embed = discord.Embed(title="후원 감사합니다", description= "```" + msg + " 후원 진심으로 감사합니다```", color=0x00D8FF)
-                        await client.get_channel(int(channel)).send(embed=embed)
+                        await client.channel.send(embed=embed)
                         await message.delete()
                     else:
                         await message.channel.send(f'> ** <@!{message.author.id}>님 명령어를 사용 할수 없습니다.**')
